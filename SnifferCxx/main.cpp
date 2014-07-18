48d5af6185b580277e11532c75ea0a7cabeb6070
Kern <gf4t47@gmail.com>
2014-07-17 17:15:00 -0700

    Initial Commit

---
+++ SnifferCxx/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SnifferCxx
+//
+//  Created by Ke Ding  on 7/17/14.
+//  Copyright (c) 2014 JPL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
