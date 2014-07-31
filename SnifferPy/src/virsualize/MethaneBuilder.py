107c723c55d19f8773eb1c3fea3aded41d041fc0
Kern <gf4t47@gmail.com>
2014-07-31 09:33:28 -0700

    a demo version

--- SnifferPy/src/virsualize/MethaneBuilder.py
+++ SnifferPy/src/virsualize/MethaneBuilder.py
@@ -29,24 +29,21 @@ def _cells_to_vector(cells):
     return xs, ys, zs, us, vs, ws
 
 
-def _build(hyps_his):
-    init = True
-    for hyps in hyps_his.hyps:
-        for hyp in hyps.hyp:
-            for cells in hyp.methene_cells:
-                xs, ys, zs, us, vs, ws = _cells_to_vector(cells)
-                if init:
-                    init = False
-                    fig = mb.quiver3d(xs, ys, zs, us, vs, ws, line_width=2, scale_mode="vector", scale_factor=0.1,
-                                      mode="2dtriangle")
-                else:
-                    ms = fig.mlab_source
-                    ms.reset(x=xs, y=ys, z=zs, u=us, v=vs, w=ws)
+def _build(vecs, fig):
+    xs, ys, zs, us, vs, ws = vecs
+
+    if not fig is None:
+        fig.mlab_source.reset(x=xs, y=ys, z=zs, u=us, v=vs, w=ws)
+    else:
+        fig = mb.quiver3d(xs, ys, zs, us, vs, ws, line_width=2, scale_mode="vector", scale_factor=0.1,
+                          mode="2dtriangle")
 
 
 def build(hyps, fig):
-    filter_hyps = filter(lambda hyp: hyp.probability >= 1.0 / len(hyps.hyp), hyps.hyp)
-    pass
-
+    cells_his_list = map(lambda hyp: hyp.methene_cells, filter(lambda hyp: hyp.probability >= 0.9 / len(hyps.hyp), hyps.hyp))
+    for cells_list in zip(*cells_his_list):
+        all_cells = reduce(lambda cells1, cells2: cells1.extend(cells2), cells_list)
+        all_vecs = _cells_to_vector(all_cells)
+        return _build(all_vecs, fig)
 
 
