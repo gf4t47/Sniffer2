019f9bdda1b012a00987aa5f82604b6c8aec083a
Kern <gf4t47@gmail.com>
2014-08-01 19:29:57 -0700

    cfg

--- bridge/script/protoc_c_py.sh
+++ bridge/script/protoc_c_py.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 /usr/local/Cellar/protobuf/2.5.0/bin/protoc -I=/Users/kding/Desktop/Sniffer2/bridge/interface/ --cpp_out=/Users/kding/Desktop/Sniffer2/SnifferCxx/SnifferCxx/filesystem /Users/kding/Desktop/Sniffer2/bridge/interface/cell.proto /Users/kding/Desktop/Sniffer2/bridge/interface/hypothesis.proto
-/usr/local/Cellar/protobuf/2.5.0/bin/protoc -I=/Users/kding/Desktop/Sniffer2/bridge/interface/  --python_out=/Users/kding/Desktop/Sniffer2/SnifferPy/src/filesystem /Users/kding/Desktop/Sniffer2/bridge/interface/cell.proto /Users/kding/Desktop/Sniffer2/bridge/interface/hypothesis.proto
+/usr/local/Cellar/protobuf/2.5.0/bin/protoc -I=/Users/kding/Desktop/Sniffer2/bridge/interface/ --python_out=/Users/kding/Desktop/Sniffer2/SnifferPy/src/filesystem /Users/kding/Desktop/Sniffer2/bridge/interface/cell.proto /Users/kding/Desktop/Sniffer2/bridge/interface/hypothesis.proto
