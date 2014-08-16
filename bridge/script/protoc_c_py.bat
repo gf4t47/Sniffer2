c40744a1b02b040b4e4851f93aada3bf319d6cd7
Kern <gf4t47@gmail.com>
2014-08-15 22:26:51 -0700

    change back to protobuf 2.5.0, reconfigure on visual studio

--- bridge/script/protoc_c_py.bat
+++ bridge/script/protoc_c_py.bat
@@ -6,5 +6,5 @@ set proto_dir=../interface
 set cxx_dir=../../SnifferCxx/SnifferCxx/protomsg
 set py_dir=../../SnifferPy/src/protomsg
 
-%protoc241% -I=%proto_dir% --cpp_out=%cxx_dir% %proto_dir%/cell.proto %proto_dir%/hypothesis.proto %proto_dir%/dect.proto
-%protoc241% -I=%proto_dir% --python_out=%py_dir% %proto_dir%/cell.proto %proto_dir%/hypothesis.proto %proto_dir%/dect.proto
+%protoc250% -I=%proto_dir% --cpp_out=%cxx_dir% %proto_dir%/cell.proto %proto_dir%/hypothesis.proto %proto_dir%/dect.proto
+%protoc250% -I=%proto_dir% --python_out=%py_dir% %proto_dir%/cell.proto %proto_dir%/hypothesis.proto %proto_dir%/dect.proto
